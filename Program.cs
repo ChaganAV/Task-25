87ef1c20b815d58f2a68a0c282bf7f5b0e3f4ea5
Alexandr <jwchaganav@gmail.com>
2022-11-08 13:01:26 +0400

    тестирование

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int size = 2;
+int[] array = new int[size];
+
+Console.WriteLine("Введите два числа");
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = ReadInline(">>");
+}
+
+Console.WriteLine($"{PowerOfNumber(array)}");
+
+int ReadInline(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PowerOfNumber(int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        result*=array[0];
+    }
+    return result;
+}
